7ae3325e50493f21a04f1215a5f31efc858e9010
belakor <davidnieto09@gmail.com>
2016-01-22 14:04:14 +0100

    Update marcosBot.php

--- Cliente/UT1/marcosBot.php
+++ Cliente/UT1/marcosBot.php
@@ -5,5 +5,5 @@ $website = "https://api.telegram.org/bot".$botToken;
 $update = file_get_contents("php://input");
 $updateArray = json_decode($update, TRUE);
 $chatId = $updateArray["result"][0]["message"]["chat"]["id"];
-file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=Estoy en el pueblo");
+file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=EstoyEnElPueblo");
 ?>
